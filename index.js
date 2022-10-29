2f5ae141206b8aab25ff2ced9036b2d601d7f322
abdulazeez abdulrasheed <rayshmanazeez@gmail.com>
2022-10-29 01:36:21 +0100

    made some changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const task = require('./index.json');
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = 5000 || process.env.PORT
+const port = process.env.PORT || 5000
 app.use(cors())
  app.use(express.json())
 
