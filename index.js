0e44b5c54672675dcd09e0427b51c630b1c02d3a
abdulazeez abdulrasheed <rayshmanazeez@gmail.com>
2022-10-29 01:51:54 +0100

    made some changes

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require("express");
 var cors = require("cors");
 const app = express();
-const task = require('./index.json');
+const task = require("./index.json");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.PORT || 5000
-app.use(cors())
- app.use(express.json())
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
 
+app.listen(port, () =>
+  console.log(`Hello world app listening on port ${port}!`)
+);
 
- app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     res.status(200).json(task);
-    } catch (err) {
+  } catch (err) {
     res.status(500).json("something went wrong");
   }
-})
-
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+});
